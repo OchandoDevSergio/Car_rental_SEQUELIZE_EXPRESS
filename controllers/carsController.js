07b6e8ed00d6155836a95e6aa71bc4816e7462b1
OchandoDevSergio <sergio.ochando.dev@gmail.com>
2023-06-19 23:50:01 +0200

    first get function done

--- controllers/carsController.js
+++ controllers/carsController.js
@@ -1,24 +1,46 @@
 
+const { Car } = require('../models');
+
 const carsController = {};
 
-carsController.getAllUsers = async (req, res) => {
+carsController.getAllCars = async (req, res) => {
 
-    return res.send("Hola, has llamado a cars con un get,,,,");
+    try {
+
+        const allCars = await Car.findAll();
+
+        return res.json({
+            success: true,
+            message: "Datos de todos los coches recuperados",
+            data: allCars,
+        });
+
+
+    } catch (error) {
+
+        return res.status(500).json({
+            success: false,
+            message: "Los datos no han podido ser recuperados",
+            error: error.message,
+        });
+
+    }
+
 }
 
-carsController.createNewUser = async (req, res) => {
+// carsController.createNewCar = async (req, res) => {
 
-    return res.send("Hola, has llamado a cars con un post,,,,");
-}
+//     return res.send("Hola, has llamado a cars con un post,,,,");
+// }
 
-carsController.modifyUser = async (req, res) => {
+// carsController.modifyCar = async (req, res) => {
 
-    return res.send("Hola, has llamado a cars con un put,,,,");
-}
+//     return res.send("Hola, has llamado a cars con un put,,,,");
+// }
 
-carsController.deleteUser = async (req, res) => {
+// carsController.deleteCar = async (req, res) => {
 
-    return res.send("Hola, has llamado a cars con un delete,,,,");
-}
+//     return res.send("Hola, has llamado a cars con un delete,,,,");
+// }
 
 module.exports = carsController;
--- views/carsRouter.js
+++ views/carsRouter.js
@@ -3,9 +3,9 @@ const router = require('express').Router();
 
 const carsController = require('../controllers/carsController');
 
-router.get('/', carsController.getAllUsers);
-router.post('/', carsController.createNewUser);
-router.put('/', carsController.modifyUser);
-router.delete('/', carsController.deleteUser);
+router.get('/', carsController.getAllCars);
+// router.post('/', carsController.createNewUser);
+// router.put('/', carsController.modifyUser);
+// router.delete('/', carsController.deleteUser);
 
 module.exports = router;
