c341841e371a3117c3aef09c1e9cfef2bfdec950
OchandoDevSergio <sergio.ochando.dev@gmail.com>
2023-07-11 05:03:44 +0200

    aded alternative (comented) to the isAdmin function in the middleware

--- middlewares/adminroleVerify.js
+++ middlewares/adminroleVerify.js
@@ -25,4 +25,26 @@ const isAdmin = (req, res, next) => {
   }
   };
   
+  // const isAdmin = (req, res, next) => {
+  //   try {
+  //     if (req.role_id !== 1) {
+  //       console.log(`El role_id es ${roleId}`)
+  //       return res.status(501).json({
+  //         success: true,
+  //         message: "No tienes permisos para realizar esta acción",
+  //       });
+  //     }
+  
+  //     next();
+  //   } catch (error) {
+  //     return res.status(500).json({
+  //       success: false,
+  //       massage: "No se ha podido realizar la acción",
+  //       error: error.message,
+  //     });
+  //   }
+  // };
+
+
+
   module.exports = isAdmin;
